f09ea411d50a77ca7ecd7e2b3c8697bed6948c34
DavidVentura07 <david.ventura.arqunam@gmail.com>
2026-08-21 14:02:21 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reserva-1.4';
+const CACHE = 'reserva-1.5';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
